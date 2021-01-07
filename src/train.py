ec7e63a9b22485ada3073a4481d2decf23c080db
xiaohuaibaoguigui <zxhcau@sina.com>
2021-01-07 11:41:47 +0800

    optimizer not save

--- src/train.py
+++ src/train.py
@@ -123,7 +123,7 @@ def run():
         resume_ep, scores = load_ckpt(modules_optims, 'logs/ckpt_ep10_re02_bs64_dropout02_GPU0,1,2,3_loss10.651553560081505_market.pth',load_to_cpu=False)
         print('Resume from EP: {}'.format(resume_ep))
         print(optimizer)
-
+    optimizer = optim.SGD(param_groups, momentum=0.9, weight_decay=5e-4)
     model_w = nn.DataParallel(model).to(DEVICE)
     m,scheduler = optim.lr_scheduler.MultiStepLR( optimizer, milestones=args.lr_schedule, gamma=0.5)
     
